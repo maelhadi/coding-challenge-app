3d9c092f28c40f8e471d32cc9b250eaa21e3f5d5
Mohamed Amine EL HADI <ma.elhadi@gmail.com>
2023-01-26 12:59:10 +0100

    Category RESTful api

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Services\CategoryService;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    private $categoryService;
+
+    public function __construct(CategoryService $categoryService)
+    {
+        $this->categoryService = $categoryService;
+    }
+    
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return $this->categoryService->getCategories();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return $this->categoryService->createCategory($request->all());
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $this->categoryService->destroyCategory($id);
+
+        return response(null, 204);
+    }
+}
